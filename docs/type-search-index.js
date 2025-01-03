1007dd36154e77e692f2c5883d0bb38f37a3477d
Mónica <monicayue@usal.es>
2025-01-03 12:38:21 +0100

    actualizado

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example","l":"Main"},{"p":"com.example","l":"Persona"}];updateSearchResults();
